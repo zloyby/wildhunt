41e57db7ad7a211bcb1aa5badad86d4cb66f30ad
zloyby@gmail.com <zloyby@gmail.com@f759842e-acba-11de-b6f4-e1be98fab775>
2013-12-04 13:11:59 +0000

    Menu

--- war/js/scrolling.js
+++ war/js/scrolling.js
@@ -1,7 +1,13 @@
 jQuery(document).ready(function ($) {
-
 	$(".scroll").click(function (event) {
 		event.preventDefault();
 		$('html,body').animate({scrollTop: $(this.hash).offset().top}, 500);
+	});
+	$('.menu').on('change', function () {
+		var url = $(this).val();
+		if (url) {
+			window.location = url;
+		}
+		return false;
 	});
 });
