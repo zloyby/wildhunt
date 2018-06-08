aa89537015c774cf685aaf67867dcba061ca9669
Eugene Kozlov <ekozlov@linius.com>
2018-06-08 16:04:44 +0200

    Lets encrypt changes

--- src/main/java/by/wildhunt/letsencrypt/LetsencryptServlet.java
+++ src/main/java/by/wildhunt/letsencrypt/LetsencryptServlet.java
@@ -1,6 +1,5 @@
 package by.wildhunt.letsencrypt;
 
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -10,8 +9,8 @@ import java.util.Map;
 
 public class LetsencryptServlet extends HttpServlet {
     static final String PREFIX = "/.well-known/acme-challenge/";
-    static final String KEY = "4JwVo7QM8UGjTY8Pp-Nu1IxvGGIidXMF0qQRDKGBEyc";
-    static final String VALUE = "4JwVo7QM8UGjTY8Pp-Nu1IxvGGIidXMF0qQRDKGBEyc.j_gc0OsBVK1hT47GVnksPMuNA5plGOCt_fXOLDSwFJY";
+    static final String KEY = "Kz7yeMLOO5XG-KuHCC547Zam6kJ6RHPv_T8lMW_54Sk";
+    static final String VALUE = "Kz7yeMLOO5XG-KuHCC547Zam6kJ6RHPv_T8lMW_54Sk.j_gc0OsBVK1hT47GVnksPMuNA5plGOCt_fXOLDSwFJY";
 
     private static final Map<String, String> challenges = new HashMap<>();
 
@@ -20,7 +19,7 @@ public class LetsencryptServlet extends HttpServlet {
     }
 
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         final String uri = req.getRequestURI();
         if (!uri.startsWith(PREFIX)) {
             resp.sendError(404);
