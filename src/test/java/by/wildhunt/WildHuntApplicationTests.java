5f837adadf6a6191998dcdfca488bed1cf3dafd3
Eugene <zloyby@gmail.com>
2026-03-26 20:21:47 +0100

    Add empty tests and version management

---
+++ src/test/java/by/wildhunt/WildHuntApplicationTests.java
@@ -0,0 +1,13 @@
+package by.wildhunt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WildHuntApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ src/test/java/by/wildhunt/WildHuntApplicationUnitTest.java
@@ -0,0 +1,12 @@
+package by.wildhunt;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class WildHuntApplicationUnitTest {
+
+    @Test
+    void simpleUnitTest() {
+        assertTrue(true, "The world is still spinning");
+    }
+}
